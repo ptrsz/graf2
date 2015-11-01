27a4dcac8eaeeb3986f5f67b79838ae93c9038ea
ptrsz <peti.szombathelyi@gmail.com>
2015-11-01 11:22:22 +0100

    Initial Commit

---
+++ graf2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  graf2
+//
+//  Created by Péter Szombathelyi on 01/11/15.
+//  Copyright © 2015 Péter Szombathelyi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
